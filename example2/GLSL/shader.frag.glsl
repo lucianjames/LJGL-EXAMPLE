9a50138f7f9d4ee382a5778996d227e5905989dc
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-27 11:53:35 +0100

    Make shader better and other stuff

--- example2/GLSL/shader.frag.glsl
+++ example2/GLSL/shader.frag.glsl
@@ -1,20 +1,23 @@
 #version 330 core
+
 uniform vec3 lightPos;
+
 out vec4 FragColor;
+
 in vec3 normal;
 in vec3 fragPos;
+in mat4 modelTransform;
+
 vec3 lightColor = vec3(1.0, 1.0, 1.0);
 vec3 objectColor = vec3(0.1, 0.5, 0.6);
-void main(){
 
+void main(){
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
-
     vec3 norm = normalize(normal);
     vec3 lightDir = normalize(lightPos - fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-
     vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
 }
--- example2/main.cpp
+++ example2/main.cpp
@@ -22,34 +22,26 @@ int main(){
 
     // === Main loop === //
     while (!glfwWindowShouldClose(window)){
-        // === Unbind/reset stuff (best practice or something) ===
-        glUseProgram(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-
-        // === Render ===
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Set the background color
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color and depth buffer
-
-        // Close window if escape is pressed
-        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){ glfwSetWindowShouldClose(window, true); }
-        
-        // Camera movement
+        // === Camera movement ===
         cam.processMovement(); // Process camera movement (WASD)
         object.setViewT(cam.getViewMatrix()); // Update the view matrix
         object.setProjectionT(cam.getPerspectiveMatrix()); // Update the projection matrix
 
-        // Rotate the suzanne :D monke spinny (or whatever object you loaded (I hope it's a suzanne))
+        // === Model movement ===
         model = glm::rotate(model, glm::radians(1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         object.setModelT(model); // Set the model matrix
 
-        // Render the object
+        // === Render ===
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Set the background color
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color and depth buffer
         object.draw();
 
         // === Swap buffers ===
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        // === Close window if escape is pressed ===
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){ glfwSetWindowShouldClose(window, true); }
     }
 
     return 0;
