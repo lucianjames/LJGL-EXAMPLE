19d8212471231fb6c3d6803b8db19b6cd528e563
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-27 21:22:15 +0100

    Many monkeys

--- example/main.cpp
+++ example/main.cpp
@@ -1,13 +1,31 @@
 #include <ljgl.hpp>
 int main(){
-    GLFWwindow* window = LJGL::init(512, 512, "LJGL Example", 3, 3);
+    GLFWwindow* window = LJGL::init(1024, 1024, "LJGL Example", 3, 3);
     LJGL::world testWorld(window);
-    unsigned int suzanne = testWorld.createAddModel_EBO("suzanne");
+    unsigned int suzanne = testWorld.createAddModel_EBO("suzanneLOW");
     testWorld.models[suzanne]->m_shader.createShader("GLSL/shader.vert.glsl", "GLSL/shader.frag.glsl");
-    testWorld.models[suzanne]->m_shader.setUniform3f("lightPos", 10.0f, 5.0f, 2.5f);
+    testWorld.models[suzanne]->m_shader.setUniform3f("lightPos", 0.0f, 0.0f, 1.0f);
+    
+    unsigned int frame = 0;
+    double startFrameTime = 0;
+
     while(!glfwWindowShouldClose(window)){
+        if(frame%100 == 0){
+            startFrameTime = glfwGetTime();
+        }
+        unsigned int newSuzanne = testWorld.createAddModel_EBO("suzanneLOW");
+        testWorld.models[newSuzanne]->m_shader.createShader("GLSL/shader.vert.glsl", "GLSL/shader.frag.glsl");
+        testWorld.models[newSuzanne]->m_shader.setUniform3f("lightPos", 0.0f, 0.0f, 1.0f);
+        testWorld.models[newSuzanne]->m_model = glm::translate(testWorld.models[newSuzanne]->m_model, glm::vec3(rand() % 40 - 20, rand() % 40 - 20, rand() % 40 - 20));
         testWorld.processInput();
         testWorld.render();
+        // Every 100 frames print the number of monkeys in the world, and the frame rate (frametime)
+        if(frame % 100 == 0){
+            std::cout << "Number of monkeys: " << testWorld.models.size() << std::endl;
+            std::cout << "Frame time: " << glfwGetTime() - startFrameTime << std::endl;
+
+        }
+        frame++;
     }
     return 0;
 }
