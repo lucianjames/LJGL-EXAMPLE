d203ad9029a03f96a5d7bd2a57ec69a5a3616b6d
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-11-25 21:51:39 +0000

    instancing

--- example/GLSL/shader.vert.glsl
+++ example/GLSL/shader.vert.glsl
@@ -3,6 +3,7 @@
 layout (location = 0) in vec3 aPos; // the position variable has attribute position 0
 layout (location = 1) in vec3 aNorm; // the normal variable has attribute position 1
 layout (location = 2) in vec2 aTexCoords; // the texture coordinates variable has attribute position 2
+layout (location = 3) in vec3 aOffset;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,7 +14,7 @@ out vec3 fragPos;
 out vec2 texCoords;
 
 void main(){
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos + aOffset, 1.0);
     normal = mat3(transpose(inverse(model))) * aNorm;
     fragPos = vec3(model * vec4(aPos, 1.0));
     texCoords = aTexCoords * 10.0;
--- example/main.cpp
+++ example/main.cpp
@@ -2,13 +2,17 @@
 int main(){
     GLFWwindow* window = LJGL::init(1024, 1024, "LJGL Example", 3, 3);
     LJGL::camera cam(window);
-    LJGL::model suzanne;
+    unsigned int maxInstanceCount = 10000;
+    LJGL::instancedModel suzanne(maxInstanceCount);
     suzanne.readVBO("normalSuzanne.vbo");
     suzanne.readEBO("normalSuzanne.ebo");
     suzanne.m_shader.createShader("GLSL/shader.vert.glsl", "GLSL/shader.frag.glsl");
     suzanne.m_shader.setUniform3f("lightPos", 1.0f, 2.0f, 4.0f);
     suzanne.m_shader.setUniform3f("objectColor", 1.0f, 0.5f, 0.25f);
     suzanne.m_texture.createTexture("cpp.png");
+    for(int i=0; i<maxInstanceCount; i++){
+        suzanne.addInstancePoint((rand() % 1000) / 100.0f, (rand() % 1000) / 100.0f, (rand() % 1000) / 100.0f);
+    }
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         cam.processInput();
